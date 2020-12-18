da0327688730379e7683ba420f0d0fa30fa769e4
Azizkhasyi11 <74176356+Azizkhasyi11@users.noreply.github.com>
2020-12-18 09:13:15 +0700

    Added ping command

--- main.py
+++ main.py
@@ -15,6 +15,17 @@ async def on_message_error(ctx, error):
 @client.command()
 async def delete(ctx, amount : int):
   await ctx.channel.purge(limit=amount)
+  
+async def ping(ctx):
+    if round(client.latency * 1000) <= 50:
+        embed=discord.Embed(title="PING", description=f":ping_pong: Pingpingpingpingping! The ping is **{round(client.latency *1000)}** milliseconds!", color=0x44ff44)
+    elif round(client.latency * 1000) <= 100:
+        embed=discord.Embed(title="PING", description=f":ping_pong: Pingpingpingpingping! The ping is **{round(client.latency *1000)}** milliseconds!", color=0xffd000)
+    elif round(client.latency * 1000) <= 200:
+        embed=discord.Embed(title="PING", description=f":ping_pong: Pingpingpingpingping! The ping is **{round(client.latency *1000)}** milliseconds!", color=0xff6600)
+    else:
+        embed=discord.Embed(title="PING", description=f":ping_pong: Pingpingpingpingping! The ping is **{round(client.latency *1000)}** milliseconds!", color=0x990000)
+    await ctx.send(embed=embed)
 
 @delete.error
 async def delete_error(ctx, error):
